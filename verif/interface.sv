c2670187fb121040de173e60615fb61af5885e4d
yonizvida-gif <yonizvida@gmail.com>
2026-05-20 13:13:01 +0300

    Add files via upload

---
+++ verif/interface.sv
@@ -0,0 +1,39 @@
+interface alu_mem_if #(parameter DATA_WIDTH = 8, parameter RES_WIDTH = 2*DATA_WIDTH, parameter ADDR_WIDTH = 2)(input logic clk, input logic reset);
+     
+    logic [RES_WIDTH-1:0]  res_out;
+    logic [DATA_WIDTH-1:0] rd_data;
+    logic [DATA_WIDTH-1:0] wr_data;
+    logic                  enable;
+    logic                  rd_wr;
+    logic [ADDR_WIDTH-1:0] addr; //4 registers
+    
+	
+		
+    	//Clocking Block 
+    clocking mon_cb @(posedge clk);
+		default input #0ns output #0ns;
+		input res_out, rd_data, wr_data, enable, rd_wr, addr;  // before posedge
+    endclocking
+
+    clocking drv_cb @(negedge clk);
+		default input #0ns output #0ns;
+		output wr_data, enable, rd_wr, addr; // after  posedge
+    endclocking
+          
+	modport dut_mp (
+        input clk, reset,
+        input  wr_data, enable, rd_wr, addr, //check reset/clk !!!!!!!!!!!!!!!
+        output res_out, rd_data
+    ); 
+	
+	
+	modport tb_mp (
+        clocking drv_cb, 
+        clocking mon_cb,
+        input clk, reset,
+        output   wr_data, enable, rd_wr, addr, //check reset/clk !!!!!!!!!!!!!!!
+        input res_out, rd_data
+    ); 
+	
+  
+endinterface
